fd8f3521c5a2a09047590afa73a13a4a408688d7
Axmadjon Isaqov <67532499+axmadjon2000@users.noreply.github.com>
2022-07-13 18:56:45 +0500

    animations

--- lib/pages/first/first_page.dart
+++ lib/pages/first/first_page.dart
@@ -3,6 +3,7 @@ import 'package:animation_last/models/user_model.dart';
 import 'package:animation_last/pages/second/second_page.dart';
 import 'package:animation_last/widgets/additional_widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
 //animations package
 class HomePage extends StatefulWidget {
@@ -29,13 +30,24 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.grey[300],
-        body: GridView.builder(
-          shrinkWrap: true,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3),
-          itemCount: myList.length,
-          itemBuilder: (context, index) =>
-              Hero(tag: myList[index].name, child: _item(myList[index])),
+        body: AnimationLimiter(
+          child: GridView.builder(
+              shrinkWrap: true,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3),
+              itemCount: myList.length,
+              itemBuilder: (context, index) =>
+                  AnimationConfiguration.staggeredGrid(
+                      position: index,
+                      columnCount: myList.length,
+                      duration: const Duration(milliseconds: 450),
+                      child: SlideAnimation(
+                        verticalOffset: 50,
+                        child: FadeInAnimation(
+                            child: Hero(
+                                tag: myList[index].name,
+                                child: _item(myList[index]))),
+                      ))),
         ),
         floatingActionButton: OpenPageWithAnimation(
           pressWidget: Container(
@@ -44,9 +56,12 @@ class _HomePageState extends State<HomePage> {
               decoration: const BoxDecoration(
                   shape: BoxShape.circle, color: Colors.blue),
               alignment: Alignment.center,
-              child: const Icon(
-                Icons.play_arrow,
-                size: 35,
+              child: const RotatedBox(
+                quarterTurns: 0,
+                child: Icon(
+                  Icons.home,
+                  size: 35,
+                ),
               )),
           openWidget: const SecondPage(
             text: 'first',
